08c8c5c20c02a8114f7258280184ca0b76cde6fa
Abdelrahman Gamal <62604408+abdogamal90@users.noreply.github.com>
2025-06-20 16:23:17 +0300

    ..

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << "ecommerce-platform-production-8d5c.up.railway.app"
+  config.hosts << "rails-production-ea21.up.railway.app"
 
   # Make code changes take effect immediately without server restart.
   config.enable_reloading = true
